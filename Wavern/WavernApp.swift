fd5da9fd4fdcc2cdc0a5dac7357f01dc8aa90511
Arrick <arrickrussellg@gmail.com>
2024-06-06 13:21:36 +0700

    Initial Commit

---
+++ Wavern/WavernApp.swift
@@ -0,0 +1,17 @@
+//
+//  WavernApp.swift
+//  Wavern
+//
+//  Created by Arrick Russell Adinoto on 06/06/24.
+//
+
+import SwiftUI
+
+@main
+struct WavernApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
