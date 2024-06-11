9d344fb5f09f48fdce0087cc99bbcccac85f0571
Paul Michael <paullmich28@gmail.com>
2024-06-11 14:36:42 +0700

    Login and SIgn Up page

--- Wavern/View/LoginView.swift
+++ Wavern/View/LoginView.swift
@@ -8,11 +8,89 @@
 import SwiftUI
 
 struct LoginView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+   @State var email: String = ""
+   @State var password: String = ""
+   
+   var body: some View {
+      VStack{
+         Spacer()
+         
+         VStack{
+            Text("Login")
+               .font(.title)
+               .fontWeight(.semibold)
+               .frame(maxWidth: .infinity, alignment: .leading)
+            
+            // MARK: Form
+            VStack {
+               VStack {
+                  Text("Email")
+                     .font(.title2)
+                     .fontWeight(.semibold)
+                     .frame(maxWidth: .infinity, alignment: .leading)
+                  
+                  TextField(text: $email) {
+                     Text("Email")
+                  }
+                  .frame(height: 52)
+                  .padding(.horizontal)
+                  .background(.neutral)
+                  .clipShape(RoundedRectangle(cornerRadius: 12))
+               }
+               .padding(.bottom, 18)
+               
+               Text("Password")
+                  .font(.title2)
+                  .fontWeight(.semibold)
+                  .frame(maxWidth: .infinity, alignment: .leading)
+               
+               TextField(text: $password) {
+                  Text("Password")
+               }
+               .frame(height: 52)
+               .padding(.horizontal)
+               .background(.neutral)
+               .clipShape(RoundedRectangle(cornerRadius: 12))
+            }
+            .padding(.top, 14)
+            .padding(.bottom, 32)
+            
+            // MARK: Button
+            VStack{
+               NavigationLink(destination: LoginView()) {
+                  Text("Login")
+                     .frame(minWidth: 0, maxWidth: .infinity)
+                     .font(.system(size: 17))
+                     .fontWeight(.semibold)
+                     .padding()
+                     .foregroundColor(.white)
+                     .background(.primaryPurple)
+                     .clipShape(RoundedRectangle(cornerRadius: 12))
+               }
+               .padding(.bottom, 16)
+               
+               HStack{
+                  Text("Don't have an account?")
+                     .fontWeight(.semibold)
+                  
+                  NavigationLink {
+                     SignUpView()
+                  } label: {
+                     Text("Sign Up")
+                  }
+                  .foregroundStyle(.primaryPurple)
+                  .fontWeight(.semibold)
+
+               }
+            }
+         }
+         .padding(.horizontal, 28)
+         
+         Spacer()
+      }
+   }
 }
 
 #Preview {
-    LoginView()
+   LoginView()
 }
--- Wavern/View/SignUpView.swift
+++ Wavern/View/SignUpView.swift
@@ -8,11 +8,123 @@
 import SwiftUI
 
 struct SignUpView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+   @State var name: String = ""
+   @State var email: String = ""
+   @State var password: String = ""
+   @State var confirmPassword: String = ""
+   
+   var body: some View {
+      VStack{
+         Spacer()
+         
+         VStack{
+            Text("Sign Up")
+               .font(.title)
+               .fontWeight(.semibold)
+               .frame(maxWidth: .infinity, alignment: .leading)
+            
+            // MARK: Form
+            VStack {
+               VStack {
+                  Text("Name")
+                     .font(.title3)
+                     .fontWeight(.semibold)
+                     .frame(maxWidth: .infinity, alignment: .leading)
+                  
+                  TextField(text: $name) {
+                     Text("Name")
+                  }
+                  .frame(height: 52)
+                  .padding(.horizontal)
+                  .background(.neutral)
+                  .clipShape(RoundedRectangle(cornerRadius: 12))
+               }
+               .padding(.bottom, 18)
+               
+               VStack {
+                  Text("Email")
+                     .font(.title3)
+                     .fontWeight(.semibold)
+                     .frame(maxWidth: .infinity, alignment: .leading)
+                  
+                  TextField(text: $email) {
+                     Text("Email")
+                  }
+                  .frame(height: 52)
+                  .padding(.horizontal)
+                  .background(.neutral)
+                  .clipShape(RoundedRectangle(cornerRadius: 12))
+               }
+               .padding(.bottom, 18)
+               
+               VStack {
+                  Text("Password")
+                     .font(.title3)
+                     .fontWeight(.semibold)
+                     .frame(maxWidth: .infinity, alignment: .leading)
+                  
+                  TextField(text: $password) {
+                     Text("Password")
+                  }
+                  .frame(height: 52)
+                  .padding(.horizontal)
+                  .background(.neutral)
+                  .clipShape(RoundedRectangle(cornerRadius: 12))
+               }
+               .padding(.bottom, 18)
+               
+               Text("Confirm Password")
+                  .font(.title3)
+                  .fontWeight(.semibold)
+                  .frame(maxWidth: .infinity, alignment: .leading)
+               
+               TextField(text: $confirmPassword) {
+                  Text("Confirm Password")
+               }
+               .frame(height: 52)
+               .padding(.horizontal)
+               .background(.neutral)
+               .clipShape(RoundedRectangle(cornerRadius: 12))
+            }
+            .padding(.top, 14)
+            .padding(.bottom, 32)
+            
+            // MARK: Button
+            VStack{
+               NavigationLink(destination: LoginView()) {
+                  Text("Login")
+                     .frame(minWidth: 0, maxWidth: .infinity)
+                     .font(.system(size: 17))
+                     .fontWeight(.semibold)
+                     .padding()
+                     .foregroundColor(.white)
+                     .background(.primaryPurple)
+                     .clipShape(RoundedRectangle(cornerRadius: 12))
+               }
+               .padding(.bottom, 16)
+               
+               HStack{
+                  Text("Have an account?")
+                     .fontWeight(.semibold)
+                  
+                  NavigationLink {
+                     LoginView()
+                  } label: {
+                     Text("Login")
+                  }
+                  .foregroundStyle(.primaryPurple)
+                  .fontWeight(.semibold)
+                  
+               }
+            }
+         }
+         .padding(.horizontal, 28)
+         
+         Spacer()
+      }
+   }
 }
 
 #Preview {
-    SignUpView()
+   SignUpView()
 }
