381361be3415d96bdb32265e0b21bab6bda6cb9f
Arrick <arrickrussellg@gmail.com>
2024-06-20 09:15:05 +0700

    Test

--- Wavern/View/TalentsProfileView.swift
+++ Wavern/View/TalentsProfileView.swift
@@ -16,18 +16,164 @@ extension UIScreen{
 struct TalentsProfileView: View {
     @Environment(\.dismiss) private var dismiss
 
+    
+    var name:String="Jesslyn Devaline"
+    var employmentType:EmploymentTypeView.employmentType = .full_time
+    
+    @State var interviewBtnColor:Color=Color.primaryPurple
+    @State var interviewTxtColor:Color=Color.white
+    @State var isInvited:Bool=false
+    
     var body: some View {
         VStack{
             Image("DummyTalentsProfile")
-            Spacer()
-            VStack{
-                HStack{
-                    Text("Jesslyn Devaline")
-                    Text(" ")
+            HStack{
+                VStack(alignment:.leading){
+                    HStack{
+                        Text("\(name)").font(.system(size:20,weight:.semibold))
+                        EmploymentTypeView(type:employmentType)
+                    }
+                    Text("Product Designer").font(.system(size:16,weight:.regular)).foregroundColor(.neutral500)
                     
+                    
+                    Text("")
+                    
+                    HStack{
+                        Image("yoe_symbol")
+                        Text("1-3 YOE").foregroundColor(.neutral400).font(.system(size:13,weight:.medium))
+                    }
+                    
+                    HStack{
+                        Image("salary_symbol")
+                        Text("Rp5.000.000").foregroundColor(.neutral400).font(.system(size:13,weight:.medium))
+                    }
+                    
+                    HStack{
+                        Image("loc_symbol")
+                        Text("Willing to Relocate").foregroundColor(.neutral400).font(.system(size:13,weight:.medium))
+                    }
                 }
+                .padding([.leading,.trailing],24)
+                .padding([.top,.bottom],20)
+                Spacer()
             }
-        }.ignoresSafeArea()
+            .frame(width:UIScreen.screenWidth).background(.white)
+
+            HStack{
+                VStack(alignment:.leading){
+                    Text("Skills").font(.system(size:20,weight:.semibold))
+                    HStack{
+                        SkillTagView("UI/UX Design")
+                        SkillTagView("Research")
+                        SkillTagView("Product Thinking")
+                    }
+                    
+                    HStack{
+                        SkillTagView("Prototyping")
+                        SkillTagView("Product Strategy")
+                        SkillTagView("Interaction Design")
+
+                    }
+                }
+                .padding(.leading,24)
+                Spacer()
+                
+            }.frame(width:UIScreen.screenWidth, height:144).background(.white)
+            
+            HStack{
+                VStack(alignment:.leading){
+                    Text("Portofolio Links").font(.system(size:20,weight:.semibold))
+                    
+                    
+                    HStack{
+                        Button(action:{
+                            
+                        },label:{
+                            PortofolioTagView(img:"email_logo")
+                        })
+
+                        Button(action:{
+                            
+                        },label:{
+                            PortofolioTagView(img:"instagram_logo")
+                        })
+
+                        
+                        Button(action:{
+                            
+                        },label:{
+                            PortofolioTagView(img:"github_logo")
+                        })
+
+                        
+                        Button(action:{
+                            
+                        },label:{
+                            PortofolioTagView(img:"dribble_logo")
+                        })
+
+                        
+                        Button(action:{
+                            
+                        },label:{
+                            PortofolioTagView(img:"figma_logo")
+                        })
+
+                        
+                        Button(action:{
+                            
+                        },label:{
+                            PortofolioTagView(img:"link_logo")
+                        })
+
+                    }
+                    
+                }.padding(.leading,24)
+                Spacer()
+                
+            }.frame(width:UIScreen.screenWidth,height:118).background(.white)
+            
+            
+            Spacer()
+
+            
+            VStack{
+                Button(action:{
+                    withAnimation{
+                        isInvited=true
+                        interviewBtnColor = .neutral300
+                        interviewTxtColor = .neutral500
+                    }
+                },label:{
+                    VStack{
+                        Text("Invite to Interview").foregroundColor(interviewTxtColor).font(.system(size:17,weight:.semibold))
+                    }.frame(width:361,height:56).background(interviewBtnColor).cornerRadius(12)
+                })
+                .padding(16)
+                Spacer()
+                
+                VStack{
+                    Text("")
+                }.frame(width:120,height:5).background(.black).cornerRadius(5).padding(10).padding(.bottom,10)
+            }.frame(width:UIScreen.screenWidth,height:128).background(.white)
+        }
+        .overlay{
+            if isInvited{
+                HStack{
+                    HStack{
+                        Image(systemName:"checkmark.circle.fill").foregroundColor(.white)
+                        Text("")
+                        Text("Request Successfully Sent").foregroundColor(.white).font(.system(size:14,weight:.medium))
+                    }
+                    .padding(.leading,16)
+                    Spacer()
+                }.frame(width:361,height:48).background(.success600).cornerRadius(12).offset(y:260)
+            }
+            
+
+        }
+        .background(.neutral)
+        .ignoresSafeArea()
             .navigationBarBackButtonHidden().toolbar{
                 HStack{
                     Button(action:{
@@ -42,7 +188,32 @@ struct TalentsProfileView: View {
     }
 }
 
+struct PortofolioTagView:View{
+    
+    var img:String
+    
+    var body : some View{
+        VStack{
+            Image(img)
+        }.frame(width:42,height:42).background(LinearGradient(colors:[.primaryPurple,.secondaryPurple],startPoint:.top,endPoint:.bottom)).cornerRadius(180)
+    }
+}
 
+
+struct SkillTagView:View{
+    var text:String
+    
+    init(_ text:String){
+        self.text=text
+    }
+    
+    var body : some View{
+        VStack{
+            Text(text).foregroundColor(.primaryPurple)
+                .font(.system(size:12,weight:.medium)).padding(8)
+        }.background(.primary100).cornerRadius(82)
+    }
+}
 
 
 struct EmploymentTypeView:View{
@@ -56,6 +227,8 @@ struct EmploymentTypeView:View{
     @State var text:String=""
     @State var bgColor:Color=Color.clear
     @State var txtColor:Color=Color.clear
+    
+    
     
     var body : some View{
         VStack{
@@ -80,10 +253,11 @@ struct EmploymentTypeView:View{
 }
 
 #Preview {
-    /*
+    
     NavigationStack{
         TalentsProfileView()
     }
-     */
-    EmploymentTypeView(type: .part_time)
+    
+    //EmploymentTypeView(type: .part_time)
+    //SkillTagView(text: "UI/UX Design")
 }
