09f8cff188244a58371d1e7358bc6a83f182e0d9
Paul Michael <paullmich28@gmail.com>
2024-06-20 10:19:26 +0700

    Talent profile

--- Wavern/View/TalentDetailView.swift
+++ Wavern/View/TalentDetailView.swift
@@ -7,195 +7,183 @@
 
 import SwiftUI
 
+extension UIScreen{
+   static let screenWidth = UIScreen.main.bounds.size.width
+   static let screenHeight = UIScreen.main.bounds.size.height
+   static let screenSize = UIScreen.main.bounds.size
+}
+
 struct TalentDetailView: View {
    @Environment(\.dismiss) var dismiss
+   
    let corners: UIRectCorner = [.topLeft, .bottomLeft, .topRight, .bottomRight]
    
+   @State var interviewBtnColor:Color = .primaryPurple
+   @State var interviewTxtColor:Color = .white
+   @State var isInvited:Bool=false
+   
    var body: some View {
-      
-      VStack(alignment: .leading) {
+      VStack{
          Image("DummyTalentsProfile")
-            .frame(width: 393, height: 269)
-            .scaledToFit()
-            .ignoresSafeArea()
-         
-         VStack(alignment: .leading) {
-            HStack {
-               Text("Jesslyn Devaline")
-                  .font(.title)
-                  .fontWeight(.semibold)
+         HStack{
+            VStack(alignment:.leading){
+               HStack{
+                  Text("Jesslyn Devaline").font(.system(size:20,weight:.semibold))
+                  EmploymentTypeView(corners: corners, type: .full_time)
+               }
+               Text("Product Designer").font(.system(size:16,weight:.regular)).foregroundColor(.neutral500)
                
-               EmploymentTypeView(corners: corners, type: .full_time)
-            }
-            
-            Text("Product Designer")
-               .font(.title2)
-               .opacity(0.5)
-            
-         }
-         .padding()
-         
-         VStack(alignment: .leading) {
-            HStack{
-               Image(systemName: "suitcase")
                
-               Text("1-3 YOE")
-            }
-            
-            HStack{
-               Image(systemName: "creditcard")
+               Text("")
                
-               Text("Rp. 5.000.000")
-            }
-            
-            HStack{
-               Image(systemName: "suitcase")
-               
-               Text("Willing to Relocate")
-            }
-         }
-         .padding(.horizontal)
-         .opacity(0.3)
-         
-         VStack(alignment: .leading){
-            Text("Skills")
-               .font(.title3)
-               .fontWeight(.semibold)
-            
-            HStack{
-               Text("UI/UX Designer")
-                  .font(.caption2)
-                  .padding(8)
-                  .background(.purple100)
-                  .foregroundStyle(.primaryPurple)
-                  .clipShape(RoundedRectangle(cornerRadius: 12))
-               
-               Text("UI/UX Designer")
-                  .font(.caption2)
-                  .padding(8)
-                  .background(.purple100)
-                  .foregroundStyle(.primaryPurple)
-                  .clipShape(RoundedRectangle(cornerRadius: 12))
-               
-               Text("UI/UX Designer")
-                  .font(.caption2)
-                  .padding(8)
-                  .background(.purple100)
-                  .foregroundStyle(.primaryPurple)
-                  .clipShape(RoundedRectangle(cornerRadius: 12))
-            }
-            
-            HStack{
-               Text("UI/UX Designer")
-                  .font(.caption2)
-                  .padding(8)
-                  .background(.purple100)
-                  .foregroundStyle(.primaryPurple)
-                  .clipShape(RoundedRectangle(cornerRadius: 12))
-               
-               Text("UI/UX Designer")
-                  .font(.caption2)
-                  .padding(8)
-                  .background(.purple100)
-                  .foregroundStyle(.primaryPurple)
-                  .clipShape(RoundedRectangle(cornerRadius: 12))
-               
-               Text("UI/UX Designer")
-                  .font(.caption2)
-                  .padding(8)
-                  .background(.purple100)
-                  .foregroundStyle(.primaryPurple)
-                  .clipShape(RoundedRectangle(cornerRadius: 12))
-            }
-         }
-         .padding()
-         
-         VStack(alignment: .leading){
-            Text("Portfolio Links")
-               .font(.title3)
-               .fontWeight(.semibold)
-            
-            HStack{
-               ZStack{
-                  Circle()
-                     .frame(width: 42, height: 42)
-                     .foregroundStyle(
-                        LinearGradient(
-                           stops: [
-                              Gradient.Stop(color: .primaryPurple, location: 0.00),
-                              Gradient.Stop(color: .secondaryPurple, location: 1.00),
-                           ],
-                           startPoint: UnitPoint(x: 0.5, y: 0),
-                           endPoint: UnitPoint(x: 0.5, y: 1)
-                        )
-                     )
+               VStack(alignment: .leading, spacing: 4){
+                  HStack{
+                     Image(systemName: "suitcase")
+                     Text("1-3 YOE").font(.system(size:13,weight:.medium))
+                  }
                   
-                  Image(systemName: "envelope")
-                     .foregroundStyle(.white)
+                  HStack{
+                     Image(systemName: "creditcard")
+                     Text("Rp5.000.000").font(.system(size:13,weight:.medium))
+                  }
+                  
+                  HStack{
+                     Image(systemName: "suitcase")
+                     Text("Willing to Relocate").font(.system(size:13,weight:.medium))
+                  }
+               }
+               .opacity(0.4)
+            }
+            .padding([.leading,.trailing],24)
+            .padding([.top,.bottom],20)
+            Spacer()
+         }
+         .frame(width:UIScreen.screenWidth).background(.white)
+         
+         HStack{
+            VStack(alignment:.leading){
+               Text("Skills").font(.system(size:20,weight:.semibold))
+               HStack{
+                  SkillTagView("UI/UX Design")
+                  SkillTagView("Research")
+                  SkillTagView("Product Thinking")
                }
                
-               ZStack{
-                  Circle()
-                     .frame(width: 42, height: 42)
-                     .foregroundStyle(
-                        LinearGradient(
-                           stops: [
-                              Gradient.Stop(color: .primaryPurple, location: 0.00),
-                              Gradient.Stop(color: .secondaryPurple, location: 1.00),
-                           ],
-                           startPoint: UnitPoint(x: 0.5, y: 0),
-                           endPoint: UnitPoint(x: 0.5, y: 1)
-                        )
-                     )
+               HStack{
+                  SkillTagView("Prototyping")
+                  SkillTagView("Product Strategy")
+                  SkillTagView("Interaction Design")
                   
-                  Image(systemName: "envelope")
-                     .foregroundStyle(.white)
-               }
-               
-               ZStack{
-                  Circle()
-                     .frame(width: 42, height: 42)
-                     .foregroundStyle(
-                        LinearGradient(
-                           stops: [
-                              Gradient.Stop(color: .primaryPurple, location: 0.00),
-                              Gradient.Stop(color: .secondaryPurple, location: 1.00),
-                           ],
-                           startPoint: UnitPoint(x: 0.5, y: 0),
-                           endPoint: UnitPoint(x: 0.5, y: 1)
-                        )
-                     )
-                  
-                  Image(systemName: "envelope")
-                     .foregroundStyle(.white)
                }
             }
-         }
-         .padding(.horizontal)
+            .padding(.leading,24)
+            Spacer()
+            
+         }.frame(width:UIScreen.screenWidth, height:144).background(.white)
          
-         Button(action: {}, label: {
-            Text("Invite to Interview")
-               .foregroundStyle(.white)
-               .font(.headline)
-               .frame(maxWidth: .infinity, minHeight: 56)
-               .background(.primaryPurple)
-               .clipShape(RoundedRectangle(cornerRadius: 12))
-         })
-         .padding()
+         HStack{
+            VStack(alignment:.leading){
+               Text("Portofolio Links").font(.system(size:20,weight:.semibold))
+               
+               
+               HStack{
+                  Button(action:{
+                     
+                  },label:{
+                     PortofolioTagView(img:"email_logo")
+                  })
+                  
+                  Button(action:{
+                     
+                  },label:{
+                     PortofolioTagView(img:"instagram_logo")
+                  })
+                  
+                  
+                  Button(action:{
+                     
+                  },label:{
+                     PortofolioTagView(img:"github_logo")
+                  })
+                  
+                  
+                  Button(action:{
+                     
+                  },label:{
+                     PortofolioTagView(img:"dribble_logo")
+                  })
+                  
+                  
+                  Button(action:{
+                     
+                  },label:{
+                     PortofolioTagView(img:"figma_logo")
+                  })
+                  
+                  
+                  Button(action:{
+                     
+                  },label:{
+                     PortofolioTagView(img:"link_logo")
+                  })
+                  
+               }
+               
+            }.padding(.leading,24)
+            Spacer()
+            
+         }.frame(width:UIScreen.screenWidth,height:118).background(.white)
          
          Spacer()
-      }
-      .ignoresSafeArea()
-      .toolbar(content: {
-         ToolbarItem(placement: .topBarLeading) {
-            Button(action: {
-               dismiss()
-            }, label: {
-               Image(systemName: "arrow.left")
+         
+         VStack{
+            Button(action:{
+               withAnimation{
+                  isInvited=true
+                  interviewBtnColor = .neutral300
+                  interviewTxtColor = .neutral500
+               }
+            },label:{
+               VStack{
+                  Text("Invite to Interview").foregroundColor(interviewTxtColor).font(.system(size:17,weight:.semibold))
+               }.frame(width:361,height:56).background(interviewBtnColor).cornerRadius(12)
             })
-            .foregroundStyle(.white)
+            .padding(16)
+            Spacer()
+            
+            VStack{
+               Text("")
+            }.frame(width:120,height:5).background(.black).cornerRadius(5).padding(10).padding(.bottom,10)
+         }.frame(width:UIScreen.screenWidth,height:128).background(.white)
+      }
+      .overlay{
+         if isInvited{
+            HStack{
+               HStack{
+                  Image(systemName:"checkmark.circle.fill").foregroundColor(.white)
+                  Text("")
+                  Text("Request Successfully Sent").foregroundColor(.white).font(.system(size:14,weight:.medium))
+               }
+               .padding(.leading,16)
+               Spacer()
+            }.frame(width:361,height:48).background(.success600).cornerRadius(12).offset(y:260)
          }
-      })
+         
+         
+      }
+      .background(.neutral)
+      .ignoresSafeArea()
+      .navigationBarBackButtonHidden().toolbar{
+         HStack{
+            Button(action:{
+               dismiss()
+            },label:{
+               Image(systemName:"arrow.backward").foregroundColor(.white)
+            }).padding([.leading,.top],24)
+            Spacer()
+         }
+         .frame(width:UIScreen.screenWidth)
+      }
    }
 }
 
